410b547b6e9ba049ccf05e5ba19672418f736c73
Simon Wessel <simon.w.karlsson@gmail.com>
2014-12-06 00:33:50 +0100

    Added generation of solvable puzzles.

--- sudoku.js
+++ sudoku.js
@@ -10,7 +10,7 @@ var block_size = 3 * cell_size;
 // Colors
 var cell_border = "#00534C";
 var cell_number = "#086B63";
-var cell_number_immutable = "#00534C";
+var cell_number_locked = "#00534C";
 var cell_number_selected = "#865800";
 var cell_number_colliding = "#840007";
 var cell_fill = "#2D9088";
@@ -21,7 +21,7 @@ var thick_lines = "#00534C";
 // Global variables
 var selection = 0;
 //var board;
-var game = {board: 'undefined'};
+var game = {board: 'undefined', locked: 'undefined'};
 var ctx;
 var boxes = [[  0,  1,  2,  9, 10, 11, 18, 19, 20],
              [  3,  4,  5, 12, 13, 14, 21, 22, 23],
@@ -33,11 +33,15 @@ var boxes = [[  0,  1,  2,  9, 10, 11, 18, 19, 20],
              [ 57, 58, 59, 66, 67, 68, 75, 76, 77],
              [ 60, 61, 62, 69, 70, 71, 78, 79, 80]];
 
-function main() {
+var main = function() {
     var canvas = document.getElementById("sudoku");
     ctx = canvas.getContext("2d");
-    createEmptyBoard(game);
-    randomizeBoard(game);
+    var solved = false;
+    while(!solved) {
+        createEmptyBoard(game);
+        solved = generateSolution(game);
+    }
+    digHoles(game);
     drawBoard(ctx, 0, 0, game);
     canvas.setAttribute("tabindex", "0");
     canvas.addEventListener("keydown", function(e) {
@@ -46,10 +50,12 @@ function main() {
     canvas.focus();
 }
 
-function drawCell(ctx, x, y, n, selected, colliding) {
+var drawCell = function(ctx, x, y, n, selected, colliding, locked) {
     ctx.lineWidth = 1;
     if(selected) {
         ctx.fillStyle = cell_fill_selected;
+    } else if(colliding && locked) {
+        ctx.fillStyle = cell_fill;
     } else if(colliding) {
         ctx.fillStyle = cell_fill_colliding;
     } else {
@@ -65,24 +71,28 @@ function drawCell(ctx, x, y, n, selected, colliding) {
         ctx.fillStyle = cell_number_colliding;
     } else if(selected) {
         ctx.fillStyle = cell_number_selected;
+    } else if(locked) {
+        ctx.fillStyle = cell_number_locked;
     } else {
         ctx.fillStyle = cell_number;
     }
     ctx.fillText(n, x + cell_size / 2, y + cell_size / 2);
 }
 
-function drawBoard(ctx, x, y, game) {
-    for(i = 0; i < 81; i++) {
-        var cellX = x + Math.floor(i / 9) * cell_size;
-        var cellY = y + (i % 9) * cell_size;
-        var number = game.board[i];
-        if(i == selection) drawCell(ctx, cellX, cellY, number, true, false);
-        else drawCell(ctx, cellX, cellY, number, false, false);
+var drawBoard = function(ctx, x, y, game) {
+    for(cell = 0; cell < 81; cell++) {
+        var cellX = x + Math.floor(cell / 9) * cell_size;
+        var cellY = y + (cell % 9) * cell_size;
+        var number = (game.board[cell] !== 0 ? game.board[cell] : "");
+        var selected = (cell == selection) ? true : false;
+        var colliding = (getCollisions(game, cell).length > 0 ? true : false);
+        var locked = (game.locked[cell] !== 0 ? true : false);
+        drawCell(ctx, cellX, cellY, number, selected, colliding, locked);
     }
     drawThickLines(ctx);
 }
 
-function drawThickLines(ctx) {
+var drawThickLines = function(ctx) {
     ctx.lineWidth = cell_size / 10;
     ctx.strokeStyle = thick_lines;
     for(i = 0; i < 4; i++) {
@@ -96,43 +106,63 @@ function drawThickLines(ctx) {
     ctx.stroke();
 }
 
-function createEmptyBoard(game) {
+var createEmptyBoard = function(game) {
     game.board = new Array(81);
-    for(i = 0; i < 81; i++) game.board[i] = "";
+    for(i = 0; i < 81; i++) game.board[i] = 0;
+    game.locked = new Array(81);
+    for(i = 0; i < 81; i++) game.locked[i] = 0;
 }
 
-function randomizeBoard(game) {
-    for(k = 0; k < 30; k++) {
-        var random_cell = Math.floor(Math.random() * 81);
-        while(game.board[random_cell] !== "") random_cell = Math.floor(Math.random() * 81);
-        if(DEBUG) console.log("Randomized cell: " + random_cell);
-        
-        var random_number = Math.floor(Math.random() * 9) + 1;
-        if(DEBUG) console.log("Generated: " + random_number);
-        game.board[random_cell] = random_number;
-        var randomize_collisions = getCollisions(game, random_cell);
-        if(DEBUG) console.log("Collisions: " + randomize_collisions);
-        while(randomize_collisions.length > 0) {
-            random_number = Math.floor(Math.random() * 9) + 1;
-            if(DEBUG) console.log("Generated new number: " + random_number);
-            game.board[random_cell] = random_number;
-            randomize_collisions = getCollisions(game, random_cell);
+var generateSolution = function(game) {
+    var untried = new Array(81);
+    for(i = 0; i < 81; i++) {
+        untried[i] = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    }
+    var i = 0;
+    while(i < 81) {
+        var nextToTry;
+        if(untried[i].length > 0) {
+            nextToTry = untried[i].splice([Math.floor(Math.random() * untried[i].length)], 1)[0];
         }
-        game.board[random_cell] = random_number;
+        game.board[i] = nextToTry;
+        if(getCollisions(game, i).length > 0) {
+            if(untried[i].length > 0) {
+                continue;
+            } else {
+                untried[i] = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+                i--;
+                if(i < 0) {
+                    return false;
+                }
+            }
+        } else {
+            i++;
+        }
+    }
+    return true;
+}
+
+var digHoles = function(game) {
+    for(i = 0; i < 30; i++) {
+        var randomPick = Math.floor(Math.random() * game.board.length);
+        game.locked[randomPick] = game.board[randomPick];
+    }
+    for(i = 0; i < 81; i++) {
+        if(game.locked[i] === 0) game.board[i] = 0;
     }
 }
 
-function getCollisions(game, check_index) {
+var getCollisions = function(game, check_index) {
     var collisions = new Array();
 
-    if(game.board[check_index] === "") return collisions;
+    if(game.board[check_index] === 0) return collisions;
 
     //check row
     var row = Math.floor(check_index / 9);
     for(i = (row * 9); i < (row * 9 + 9); i++) {
         if(i != check_index &&
            game.board[i] === game.board[check_index] &&
-           game.board[check_index] != "") {
+           game.board[check_index] !== 0) {
             if(collisions.indexOf(i) === -1) {
                 if(DEBUG) console.log("Check row: Adding " + i + " to collisions.");
                 collisions.push(i);
@@ -145,7 +175,7 @@ function getCollisions(game, check_index) {
     for(i = col; i < 73 + col; i += 9) {
         if(i != check_index &&
            game.board[i] === game.board[check_index] &&
-           game.board[check_index] != "") {
+           game.board[check_index] !== 0) {
             if(collisions.indexOf(i) == -1) {
                 if(DEBUG) console.log("Check column: Adding " + i + " to collisions.");
                 collisions.push(i);
@@ -166,7 +196,7 @@ function getCollisions(game, check_index) {
         var box_cell = boxes[box_index][j];
         if(box_cell != check_index &&
            game.board[box_cell] === game.board[check_index] &&
-           game.board[check_index] != "") {
+           game.board[check_index] !== 0) {
             if(collisions.indexOf(box_cell) == -1) {
                 if(DEBUG) console.log("Check box: Adding " + box_cell + " to collisions.");
                 collisions.push(box_cell);
@@ -177,7 +207,7 @@ function getCollisions(game, check_index) {
     return collisions;
 }
 
-function checkAll(game) {
+var checkAll = function(game) {
     var ok = true;
     for(p = 0; p < 81; p++) {
         var collisions = getCollisions(game, p);
@@ -190,7 +220,7 @@ function checkAll(game) {
     return ok;
 }
 
-function processKeys(e, game) {
+var processKeys = function(e, game) {
     switch(e.keyCode) {
         case 38:
             if(DEBUG) console.log("Pressed up");
@@ -214,7 +244,7 @@ function processKeys(e, game) {
             break;
         case 46:
             if(DEBUG) console.log("Pressed delete");
-            game.board[selection] = "";
+            if(game.locked[selection] === 0) game.board[selection] = 0;
             break;
         case 67:
             if(DEBUG) console.log("Pressed C");
@@ -223,7 +253,7 @@ function processKeys(e, game) {
     }
     if(e.keyCode >= 49 && e.keyCode <= 57) {
             if(DEBUG) console.log("Pressed " + String.fromCharCode(e.keyCode));
-            game.board[selection] = parseInt(String.fromCharCode(e.keyCode));
+            if(game.locked[selection] === 0) game.board[selection] = parseInt(String.fromCharCode(e.keyCode));
             if(DEBUG) console.log(game.board);
     }
     if(DEBUG) console.log("Selection: " + selection);
@@ -231,7 +261,6 @@ function processKeys(e, game) {
     if(DEBUG) console.log("Collisions: " + getCollisions(game, selection));
     drawBoard(ctx, 0, 0, game);
     drawThickLines(ctx);
-    console.log(e.keyCode);
 }
 
 main();
